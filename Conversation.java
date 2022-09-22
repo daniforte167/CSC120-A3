abac0331a045ed9124dfdeff42c7075a75eca067
Dani Forte <daniforte167@gmail.com>
2022-09-22 17:06:37 -0400

    hi

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,33 @@
+import java.util.scanner;
+import java.util.List;
+
 class Conversation {
 
-  public static void main(String[] arguments) {
-    // You will start the conversation here.
+  public static void main(String[] arguments) {    
+    // You will start the conversation 
+    
+
+      Scanner input = new Scanner(system.in);
+      List <String> inputs = new ArrayList<String>()
+
+      //(can use same scanner - don’t need new one every input from user)"""
+      System.out.println(“Enter an integer: ”);
+      int numRounds = input.nextInt();
+      System.out.println("Hi! What's on your mind!");
+        for (int i=0; i<numRounds; i+=1){
+          String input = inputs.nextLine();
+          //that is what's prompting the users to respon
+          //adding to a list so we can print later
+          //this does it
+          inputs.add(input);
+          //this is for later step where we print out the list
+          System.out.println("Cool");
+        }
+      
+      List <String> closings = new ArrayList<String>(List.of)
+      public String[] split(String regex)
+        
+
+      
   }
-}
+} 
