2d788b5702c9dc1c5fb7c910f9c73c6054e7b019
Dani Forte <daniforte167@gmail.com>
2022-10-04 10:37:49 -0400

    https://github.com/daniforte167/A3

--- Conversation.java
+++ Conversation.java
@@ -1,33 +1,99 @@
-import java.util.scanner;
-import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 class Conversation {
 
   public static void main(String[] arguments) {    
     // You will start the conversation 
-    
+    Scanner input = new Scanner(System.in);
 
-      Scanner input = new Scanner(system.in);
-      List <String> inputs = new ArrayList<String>()
-
-      //(can use same scanner - don’t need new one every input from user)"""
-      System.out.println(“Enter an integer: ”);
-      int numRounds = input.nextInt();
-      System.out.println("Hi! What's on your mind!");
-        for (int i=0; i<numRounds; i+=1){
-          String input = inputs.nextLine();
-          //that is what's prompting the users to respon
-          //adding to a list so we can print later
-          //this does it
-          inputs.add(input);
-          //this is for later step where we print out the list
-          System.out.println("Cool");
-        }
       
-      List <String> closings = new ArrayList<String>(List.of)
-      public String[] split(String regex)
+      ArrayList<String> responses = new ArrayList<String>();
+      responses.add("Wow");
+      responses.add("Cool");
+      responses.add("Oh, yeah, definitely");
+      responses.add("Uh, huh");
+      
+  
+
+    ArrayList<String> user_inputs_responses = new ArrayList<String>(); 
+    //variable named
+    //this is what's storing all the above randomized responses
+
+
         
+        System.out.println("Enter an integer: ");
+        int numRounds = input.nextInt();
+        System.out.println("Hi! What's on your mind!");
+        input.nextLine();
+        for (int i=0; i<numRounds; i++) {
+            //this asks for the number of rounds and asks for the loop to start
+            String user_inputs = input.nextLine(); 
+            //this prompts the response
+            //System.out.println(user_inputs);4
 
-      
+            //this prints the response
+            user_inputs_responses.add(user_inputs);
+            //this calls in and storess the users responses 
+            if (user_inputs.contains("me")) {
+                String mirrored_phrase = wordMirror2(user_inputs);
+                System.out.println(mirrored_phrase + "?");
+                user_inputs_responses.add(mirrored_phrase);}
+              else if (user_inputs.contains("you")){
+                String mirrored_phrase = wordMirror2(user_inputs);
+                System.out.println(mirrored_phrase + "?");
+                user_inputs_responses.add(mirrored_phrase);}
+              else if (user_inputs.contains("I")){
+                String mirrored_phrase = wordMirror2(user_inputs);
+                System.out.println(mirrored_phrase + "?");
+                user_inputs_responses.add(mirrored_phrase);}
+              else if (user_inputs.contains("am")){
+                String mirrored_phrase = wordMirror2(user_inputs);
+                System.out.println(mirrored_phrase +"?");
+                user_inputs_responses.add(mirrored_phrase);}
+              else if (user_inputs.contains("my")){
+                String mirrored_phrase = wordMirror2(user_inputs);
+                System.out.println(mirrored_phrase + "?");
+                user_inputs_responses.add(mirrored_phrase);}
+              else if (user_inputs.contains("your")){
+                String mirrored_phrase = wordMirror2(user_inputs);
+              System.out.println(mirrored_phrase + "?");
+              user_inputs_responses.add(mirrored_phrase);}              
+              else System.out.println(responses.get(i));
+            
+
+
+
+
+//queues so it's ready for input response
+          }input.close();
+  // this should ask how many rounds there are
+  System.out.println("Goodbye now, friend!");
+
+        System.out.println(user_inputs_responses);
+
   }
-} 
+
+  public static String wordMirror2(String phrase) {
+    String mirrored_phrase = phrase.replace("I", "you");
+    mirrored_phrase = mirrored_phrase.replace("me", "you");
+    mirrored_phrase = mirrored_phrase.replace("am", "are");
+    mirrored_phrase = mirrored_phrase.replace("my", "your");
+
+    return mirrored_phrase;
+  }
+  public static String wordMirror3(String phrase_2) {
+    String mirrored_phrase_2 = phrase_2.replace("you", "I");
+    mirrored_phrase_2 = mirrored_phrase_2.replace("your", "my");
+  
+    return mirrored_phrase_2;
+  }
+}
+
+// (can use same scanner - don’t need new one every input from user)"""
+
+// needed so you declare the string
+// that is what's prompting the users to respon
+// adding to a list so we can print later
+// this does it
+// this is for later step where we print out the list
--- reflection.md
+++ reflection.md
@@ -1,7 +1,12 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+I consulted with Vivian Wei and Jillian Lehosky.
 - Which session(s) of TA / office hours did you attend?
+I went to Thursday morning office hours with Professor Crouser!
 - What are your initial impressions of Java? 
+I don't like it. It just feels very extra and I liked the straight forwardness of Python better.
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+Even if there are differences in the methods or functions used, they still seem to follow a similar structure of gather the information, organize that information, call whatever information is needed, and complete whatever function or method is necessary for the program, and print the result that is requested. 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+Definitely give yourself time to really work out the issues. There were a few times where I would just get "error" or wouldn't get any message at all but it wasn't working the wy it should and I couldn't figure it out why. I needed to re-look at it and go over it several times before it started to work properly. Working with other students definitely helps and I would recommend doing it more, because having very few collaborating times isn't helpful!
